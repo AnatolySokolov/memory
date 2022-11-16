dd7098b46657458058d650792cd294f730a98c78
Anatoly Sokolov <anatoliysokolov1981@gmail.com>
2022-11-16 15:01:59 +0300

    start page layout

---
+++ src/scripts/templates/pages/index.js
@@ -0,0 +1,9 @@
+import startGameImg from '../../../images/StartGame.png';
+
+export default `
+  <div class="container container--jc">
+    <h1 class="title">Memory game</h1>
+    <img class="start-game-image" src=${startGameImg} width="501" height="257" alt="Начать игру">
+    <a class="link link--mb20" name="game">Начать игру</a>
+  </div>
+`.trim();
