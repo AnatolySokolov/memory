75852521ee5411b1eb0fe5d542c8ab129cff10c3
Anatoly Sokolov <anatoliysokolov1981@gmail.com>
2022-11-17 11:59:30 +0300

    game page layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './styles/style.scss';
 import './favicons/favicon.ico';
+import './images/cards/index';
+
+function onCardClick(e) {
+  if (e.target.tagName !== 'IMG') return;
+
+  const card = e.target.closest('.card');
+
+  card.classList.toggle('card--is-flipped');
+}
+
+const table = document.querySelector('.game-table');
+
+table.addEventListener('click', (e) => onCardClick(e));
